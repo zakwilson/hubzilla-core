0085b64b2bb22cf463ec66376d06d64d5b5b5730
git-marijus <mario@mariovavti.com>
2017-08-06 21:40:39 +0200

    consolidate disable_discover_tab config if it was not yet set to either 1 or 0 in DB

--- include/conversation.php
+++ include/conversation.php
@@ -1622,11 +1622,9 @@ function network_tabs() {
 	// tabs
 	$tabs = array();
 
-	$d = get_config('system','disable_discover_tab');
-	if($d === false)
-		$d = 1;
+	$disable_discover_tab = get_config('system','disable_discover_tab') || get_config('system','disable_discover_tab') === false;
 
-	if(! $d) {
+	if(! $disable_discover_tab) {
 		$tabs[] = array(
 			'label' => t('Discover'),
 			'url' => z_root() . '/' . $cmd . '?f=&fh=1' ,

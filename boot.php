8b5a190d71342cbeac50c42e30cdcef24831b0df
git-marijus <mario@mariovavti.com>
2017-08-12 10:45:45 +0200

    RC1

--- boot.php
+++ boot.php
@@ -49,7 +49,7 @@ require_once('include/hubloc.php');
 require_once('include/attach.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '2.6RC' );
+define ( 'STD_VERSION',             '2.6RC1' );
 define ( 'ZOT_REVISION',            '1.2' );
 
 define ( 'DB_UPDATE_VERSION',       1193  );

dd3ec468f65f05c0fbf233a161cffae2698bd3db
Mario Vavti <mario@mariovavti.com>
2018-10-06 23:05:45 +0200

    fix issue #1281 - hubloc default values

--- boot.php
+++ boot.php
@@ -54,7 +54,7 @@ define ( 'STD_VERSION',             '3.9' );
 define ( 'ZOT_REVISION',            '6.0a' );
 
 
-define ( 'DB_UPDATE_VERSION',       1223 );
+define ( 'DB_UPDATE_VERSION',       1224 );
 
 define ( 'PROJECT_BASE',   __DIR__ );
 

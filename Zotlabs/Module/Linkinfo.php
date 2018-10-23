f180558041507bcc4f80e8f373ca3e6bd635edea
Max Kostikov <max@kostikov.co>
2018-10-23 19:32:36 +0200

    Convert non UTF-8 content on link embeding

--- Zotlabs/Module/Linkinfo.php
+++ Zotlabs/Module/Linkinfo.php
@@ -228,8 +228,10 @@ class Linkinfo extends \Zotlabs\Web\Controller {
 	
 		$header = $result['header'];
 		$body   = $result['body'];
-	
-		$body   = mb_convert_encoding($body, 'UTF-8', 'UTF-8');
+		
+		$x = preg_match('/<meta\s+http-equiv=.content-type.+; charset=([^"|\']+)/i', $body, $o);
+		
+		$body   = mb_convert_encoding($body, 'UTF-8', ($o[1] ? $o[1] : 'UTF-8'));
 		$body   = mb_convert_encoding($body, 'HTML-ENTITIES', "UTF-8");
 	
 		$doc    = new \DOMDocument();

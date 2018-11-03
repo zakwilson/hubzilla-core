46b0510b994fb885cd3c64b703ce73c2cee6d6c1
Max Kostikov <max@kostikov.co>
2018-11-03 10:41:31 +0100

    Update Linkinfo.php

--- Zotlabs/Module/Linkinfo.php
+++ Zotlabs/Module/Linkinfo.php
@@ -230,7 +230,7 @@ class Linkinfo extends \Zotlabs\Web\Controller {
 		$body   = $result['body'];
 		
 		// Check codepage in HTTP headers or HTML if not exist
-		$cp = (preg_match('/Content-Type: text\/html; charset=(.+)/i', $header, $o) ? trim($o[1]) : '');
+		$cp = (preg_match('/Content-Type: text\/html; charset=(.+)\r\n/i', $header, $o) ? $o[1] : '');
 		if(empty($cp))
 		    $cp = (preg_match('/meta.+content=["|\']text\/html; charset=([^"|\']+)/i', $body, $o) ? $o[1] : 'AUTO');
 

48717dff00a906b8ba8d70b667298fde94492b4f
zotlabs <mike@macgirvin.com>
2018-11-14 16:56:07 -0800

    change db update counter

--- boot.php
+++ boot.php
@@ -53,7 +53,7 @@ define ( 'PLATFORM_NAME',           'hubzilla' );
 define ( 'STD_VERSION',             '3.9.5' );
 define ( 'ZOT_REVISION',            '6.0a' );
 
-define ( 'DB_UPDATE_VERSION',       1225 );
+define ( 'DB_UPDATE_VERSION',       1228 );
 
 define ( 'PROJECT_BASE',   __DIR__ );
 

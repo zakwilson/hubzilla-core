238a616292ad5c6e7be97a4dd763a302729abf80
Max Kostikov <max@kostikov.co>
2018-11-16 23:33:45 +0100

    Add filesize for streaming content

--- Zotlabs/Module/Photo.php
+++ Zotlabs/Module/Photo.php
@@ -167,6 +167,7 @@ class Photo extends \Zotlabs\Web\Controller {
 					$modified = strtotime($e[0]['edited'] . 'Z');
 					if(intval($e[0]['os_storage'])) {
 						$streaming = $data;
+						$filesize = filesize($data);
 					}
 					else
 					    $filesize = $e[0]['filesize'];

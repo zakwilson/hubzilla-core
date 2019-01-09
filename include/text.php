90b6afe06bacd5377c638c89045801296d69ec44
Mario Vavti <mario@mariovavti.com>
2019-01-10 00:29:05 +0100

    we must sslify all possible content variables

--- include/text.php
+++ include/text.php
@@ -1691,7 +1691,14 @@ function prepare_body(&$item,$attach = false,$opts = false) {
 	if(local_channel() == $item['uid'])
 		$filer = format_filer($item);
 
-	$s = sslify($s);
+	if($s)
+		$s = sslify($s);
+
+	if($photo)
+		$photo = sslify($photo);
+
+	if($event)
+		$event = sslify($event);
 
 	$prep_arr = array(
 		'item' => $item,

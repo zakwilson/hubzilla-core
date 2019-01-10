6791b05a4032a076651f7c8e4790614f0f405a55
DM42.Net (Matt Dent) <dentm42@dm42.net>
2019-01-10 15:29:24 -0500

    Fix for PGSQL/MYSQL difference

--- Zotlabs/Daemon/Queue.php
+++ Zotlabs/Daemon/Queue.php
@@ -12,6 +12,15 @@ class Queue {
 		require_once('include/items.php');
 		require_once('include/bbcode.php');
 
+                switch (DBTYPE_ACTIVE) {
+                        case DBTYPE_MYSQL:
+				$sqlrandfunc = "RAND()";
+                                break;
+
+                        case DBTYPE_POSTGRESQL:
+				$sqlrandfunc = "RANDOM()";
+                                break;
+                }
 
 		if($argc > 1)
 			$queue_id = $argv[1];
@@ -62,14 +71,14 @@ class Queue {
 			// the site is permanently down, there's no reason to attempt delivery at all, or at most not more than once 
 			// or twice a day. 
 
-			$r = q("SELECT *,RAND() as rn FROM outq WHERE outq_delivered = 0 and outq_scheduled < %s order by rn limit 1",
+			$r = q("SELECT *,$sqlrandfunc as rn FROM outq WHERE outq_delivered = 0 and outq_scheduled < %s order by rn limit 1",
 				db_utcnow()
 			);
 			while ($r) {
 				foreach($r as $rv) {
 					queue_deliver($rv);
 				}
-				$r = q("SELECT *,RAND() as rn FROM outq WHERE outq_delivered = 0 and outq_scheduled < %s order by rn limit 1",
+				$r = q("SELECT *,$sqlrandfunc as rn FROM outq WHERE outq_delivered = 0 and outq_scheduled < %s order by rn limit 1",
 					db_utcnow()
 				);
 			}

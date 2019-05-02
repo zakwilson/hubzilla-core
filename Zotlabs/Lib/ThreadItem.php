47f75ad8bfbe8325a76ae6fe466aa8080fc87045
Max Kostikov <max@kostikov.co>
2019-05-02 11:28:29 +0200

    Add check if threads are enabled globally

--- Zotlabs/Lib/ThreadItem.php
+++ Zotlabs/Lib/ThreadItem.php
@@ -38,7 +38,7 @@ class ThreadItem {
 				
 		$this->data = $data;
 		$this->toplevel = ($this->get_id() == $this->get_data_value('parent'));
-		$this->threaded = feature_enabled($conv->get_profile_owner(),'reply_to');
+		$this->threaded = get_config('system','thread_allow');
 
 		$observer = \App::get_observer();
 

decc14c3244cb55cdc79fac85c4c979b014f2b75
Mario <mario@mariovavti.com>
2019-11-23 20:41:20 +0000

    sse: more cleanup

--- view/js/main.js
+++ view/js/main.js
@@ -536,10 +536,8 @@ function markRead(notifType) {
 	$.get('ping?f=&markRead='+notifType);
 	$('.' + notifType + '-button').hide();
 	$('#nav-' + notifType + '-sub').removeClass('show');
-	sessionStorage.removeItem(notifType + '_notifications_cache');
 	sessionStorage.removeItem('notification_open');
-	if(timer) clearTimeout(timer);
-	timer = setTimeout(updateInit,2000);
+	sse_setNotificationsStatus();
 }
 
 function markItemRead(itemId) {

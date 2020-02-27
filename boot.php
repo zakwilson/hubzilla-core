e7e79f742387eb2aaeb5c3688401d1df2f5c86bc
Mario <mario@mariovavti.com>
2020-02-27 08:38:01 +0000

    bump version

--- boot.php
+++ boot.php
@@ -50,7 +50,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '4.7.1' );
+define ( 'STD_VERSION',             '4.7.2' );
 define ( 'ZOT_REVISION',            '6.0a' );
 
 define ( 'DB_UPDATE_VERSION',       1235 );

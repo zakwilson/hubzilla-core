266dcd44bb5e590986b5e9ed5afb8e0106a129db
Mario <mario@mariovavti.com>
2020-05-07 20:49:52 +0000

    tests: declare return type

--- tests/unit/Photo/PhotoGdTest.php
+++ tests/unit/Photo/PhotoGdTest.php
@@ -26,7 +26,7 @@ class PhotoGdTest extends UnitTestCase {
 	/**
 	 * Prepares the environment before running a test.
 	 */
-	protected function setUp() {
+	protected function setUp(): void {
 		parent::setUp();
 
 		$data = file_get_contents('images/hz-16.png');

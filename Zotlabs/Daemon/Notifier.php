902151918c46e4ff9a40c5dc66a163aac0267f8e
Mario <mario@mariovavti.com>
2020-10-06 10:03:57 +0000

    5.0RC testing: do not attempt zot6 delivery of anonymous comments - the author is not a valid actor

--- Zotlabs/Daemon/Notifier.php
+++ Zotlabs/Daemon/Notifier.php
@@ -294,6 +294,12 @@ class Notifier {
 			$r = fetch_post_tags($r);
 		
 			$target_item = $r[0];
+
+			if($target_item['author']['xchan_network'] === 'anon') {
+				logger('notifier: target item author is not a fetchable actor', LOGGER_DEBUG);
+				return;
+			}
+
 			$deleted_item = false;
 
 			if(intval($target_item['item_deleted'])) {

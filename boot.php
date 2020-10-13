1f41b1837f5b8722b198ea808637108a83ff7f20
Mario <mario@mariovavti.com>
2020-10-13 08:33:22 +0000

    version 5.0RC5

--- boot.php
+++ boot.php
@@ -50,7 +50,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '5.0RC4' );
+define ( 'STD_VERSION',             '5.0RC5' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1238 );

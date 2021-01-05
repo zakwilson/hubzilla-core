99bfc3aaa0a54a0e8cac82a7d6d7fc8e49c046dd
Mario <mario@mariovavti.com>
2021-01-05 08:48:30 +0000

    version 5.3

--- boot.php
+++ boot.php
@@ -50,7 +50,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '5.2RC' );
+define ( 'STD_VERSION',             '5.3' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1240 );

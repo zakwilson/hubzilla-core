f1ac5bb667164a93f4969c254aa2010e7c368dd8
Mario <mario@mariovavti.com>
2021-01-18 14:09:48 +0000

    more cleanup daemon/gprobe

--- Zotlabs/Daemon/Gprobe.php
+++ Zotlabs/Daemon/Gprobe.php
@@ -9,27 +9,27 @@ use Zotlabs\Lib\Zotfinger;
 // performs zot_finger on $argv[1], which is a hex_encoded webbie/reddress
 
 class Gprobe {
-	static public function run($argc,$argv) {
+	static public function run($argc, $argv) {
 
-		if($argc != 2)
+		if ($argc != 2)
 			return;
 
 		$url = hex2bin($argv[1]);
 
-		if(! strpos($url,'@'))
+		if (!strpos($url, '@'))
 			return;
 
 		$r = q("select * from hubloc where hubloc_addr = '%s' and hubloc_network = 'zot6' limit 1",
 			dbesc($url)
 		);
 
-		if(! $r) {
+		if (!$r) {
 			$href = Webfinger::zot_url(punify($url));
-			if($href) {
+			if ($href) {
 				$zf = Zotfinger::exec($href, null);
 			}
-			if(is_array($zf) && array_path_exists('signature/signer',$zf) && $zf['signature']['signer'] === $href && intval($zf['signature']['header_valid'])) {
-				$xc = Libzot::import_xchan($zf['data']);
+			if (is_array($zf) && array_path_exists('signature/signer', $zf) && $zf['signature']['signer'] === $href && intval($zf['signature']['header_valid'])) {
+				Libzot::import_xchan($zf['data']);
 			}
 		}
 

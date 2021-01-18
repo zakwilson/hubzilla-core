45350179b4ad48b8fc81e23722974d95f39a1fe2
Mario <mario@mariovavti.com>
2021-01-18 13:41:34 +0000

    cleanup daemon/deliver

--- Zotlabs/Daemon/Deliver.php
+++ Zotlabs/Daemon/Deliver.php
@@ -5,25 +5,25 @@ namespace Zotlabs\Daemon;
 require_once('include/queue_fn.php');
 
 class Deliver {
-	
-	static public function run($argc,$argv) {
 
-		if($argc < 2)
+	static public function run($argc, $argv) {
+
+		if ($argc < 2)
 			return;
 
-		logger('deliver: invoked: ' . print_r($argv,true), LOGGER_DATA);
+		logger('deliver: invoked: ' . print_r($argv, true), LOGGER_DATA);
 
-		for($x = 1; $x < $argc; $x ++) {
+		for ($x = 1; $x < $argc; $x++) {
 
-			if(! $argv[$x])
+			if (!$argv[$x])
 				continue;
 
 			$r = q("select * from outq where outq_hash = '%s'",
 				dbesc($argv[$x])
 			);
 
-			if($r) {
-				queue_deliver($r[0],true);
+			if ($r) {
+				queue_deliver($r[0], true);
 			}
 
 		}

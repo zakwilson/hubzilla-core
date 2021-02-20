a84c7271f2267af21120a744f4836acd33c8a1e7
Max Kostikov <max@kostikov.co>
2021-02-20 14:49:31 +0100

    Formatting

--- Zotlabs/Update/_1243.php
+++ Zotlabs/Update/_1243.php
@@ -5,12 +5,12 @@ namespace Zotlabs\Update;
 class _1243 {
 
 	function run() {
-	
-        $x = get_config('system','filesystem_storage_thumbnails');
-        del_config('system','filesystem_storage_thumbnails');
+		
+		$x = get_config('system','filesystem_storage_thumbnails');
+		del_config('system','filesystem_storage_thumbnails');
 		if ($x !=== false)
-            set_config('system','photo_storage_type', intval($x));
-
+			set_config('system','photo_storage_type', intval($x));
+			
 		return UPDATE_SUCCESS;
 	}
 

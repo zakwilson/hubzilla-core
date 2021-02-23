fa73c8d15ede12a963fbf4b1efaf58c1e47dc873
Mario <mario@mariovavti.com>
2021-02-23 10:57:37 +0000

    bump dev version to 5.5

--- boot.php
+++ boot.php
@@ -52,7 +52,7 @@ require_once('include/attach.php');
 require_once('include/bbcode.php');
 
 define ( 'PLATFORM_NAME',           'hubzilla' );
-define ( 'STD_VERSION',             '5.4RC' );
+define ( 'STD_VERSION',             '5.5' );
 define ( 'ZOT_REVISION',            '6.0' );
 
 define ( 'DB_UPDATE_VERSION',       1243 );

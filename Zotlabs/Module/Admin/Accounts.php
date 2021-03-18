7c620cbe244aac5cce5d86bd3776ca85353e7f74
Mario <mario@mariovavti.com>
2021-03-18 14:42:30 +0000

    air: make sure to display the timezone corrected times in the admin ui

--- Zotlabs/Module/Admin/Accounts.php
+++ Zotlabs/Module/Admin/Accounts.php
@@ -196,6 +196,10 @@ class Accounts {
 					$pending[$n]['reg_atip'] = $v['reg_atip'] . ' ◄' . $atipn[ $v['reg_atip'] ] . '×';
 				}
 
+				$pending[$n]['reg_created'] = datetime_convert('UTC', date_default_timezone_get(), $pending[$n]['reg_created']);
+				$pending[$n]['reg_startup'] = datetime_convert('UTC', date_default_timezone_get(), $pending[$n]['reg_startup']);
+				$pending[$n]['reg_expires'] = datetime_convert('UTC', date_default_timezone_get(), $pending[$n]['reg_expires']);
+
 				$pending[$n]['status'] = t('Not yet verified');
 				if($pending[$n]['reg_vfd'])
 					$pending[$n]['status'] = t('Verified');

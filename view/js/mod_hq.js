ec22ca75536e4c85b7a2721154a9d4e7148b2ffc
Mario <mario@mariovavti.com>
2021-06-18 09:58:00 +0000

    css and js fixes

--- view/js/mod_hq.js
+++ view/js/mod_hq.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 	$(document).on('click', '#jot-toggle', function(e) {
 		$(window).scrollTop(0);
 		$('#jot-popup').toggle();
+		$('#profile-jot-text').focus();
 	});
 
 	$(document).on('click', '#notes-toggle', function(e) {
 		$(window).scrollTop(0);
 		$('#personal-notes').toggleClass('d-none');
+		$('#note-text').focus();
 	});
 
 });

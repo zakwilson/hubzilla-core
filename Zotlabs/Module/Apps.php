2e7a915ee8d99970a32e02bf3ac5bb5834f7fa1a
Mario <mario@mariovavti.com>
2021-08-25 10:19:20 +0000

    add appstore widget to appman

--- Zotlabs/Module/Apps.php
+++ Zotlabs/Module/Apps.php
@@ -9,7 +9,7 @@ class Apps extends \Zotlabs\Web\Controller {
 	function get() {
 
 		nav_set_selected('Apps');
-	
+
 		if(argc() == 2 && argv(1) == 'edit')
 			$mode = 'edit';
 		else
@@ -18,9 +18,9 @@ class Apps extends \Zotlabs\Web\Controller {
 		$available = ((argc() == 2 && argv(1) === 'available') ? true : false);
 
 		$_SESSION['return_url'] = \App::$query_string;
-	
+
 		$apps = array();
-	
+
 		if(local_channel()) {
 			Zlib\Apps::import_system_apps();
 			$syslist = array();
@@ -37,9 +37,9 @@ class Apps extends \Zotlabs\Web\Controller {
 			$syslist = Zlib\Apps::get_system_apps(true);
 
 		usort($syslist,'Zotlabs\\Lib\\Apps::app_name_compare');
-	
+
 	//	logger('apps: ' . print_r($syslist,true));
-	
+
 		foreach($syslist as $app) {
 			$apps[] = Zlib\Apps::app_render($app,(($available) ? 'install' : $mode));
 		}
@@ -53,7 +53,7 @@ class Apps extends \Zotlabs\Web\Controller {
 			'$manage' => (($available) ? '' : t('Manage Apps')),
 			'$create' => (($mode == 'edit') ? t('Create Custom App') : '')
 		));
-	
+
 	}
-	
+
 }
--- Zotlabs/Widget/Appstore.php
+++ Zotlabs/Widget/Appstore.php
@@ -6,12 +6,11 @@ namespace Zotlabs\Widget;
 class Appstore {
 
 	function widget($arr) {
-		$store = ((argc() > 1 && argv(1) === 'available') ? 1 : 0);
-		return replace_macros(get_markup_template('appstore.tpl'), [ 
+		return replace_macros(get_markup_template('appstore.tpl'), [
 			'$title' => t('App Collections'),
 			'$options' => [
-				[ z_root() . '/apps',           t('Installed apps'), 1 - $store ],
-				[ z_root() . '/apps/available', t('Available Apps'), $store ]
+				[z_root() . '/apps',           t('Installed apps'), ((argc() == 1 && argv(0) === 'apps') ? 1 : 0)],
+				[z_root() . '/apps/available', t('Available Apps'), ((argc() > 1 && argv(1) === 'available') ? 1 : 0)]
 			]
 		]);
 	}

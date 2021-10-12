ba6f0699972af6e984802cb0460c8fecd43afa59
Mario <mario@mariovavti.com>
2021-10-12 19:05:21 +0000

    HTTPSig: do not query for hubloc_addr

--- Zotlabs/Web/HTTPSig.php
+++ Zotlabs/Web/HTTPSig.php
@@ -281,8 +281,7 @@ class HTTPSig {
 
 		// $force is used to ignore the local cache and only use the remote data; for instance the cached key might be stale
 		if (!$force) {
-			$x = q("select * from xchan left join hubloc on xchan_hash = hubloc_hash where (hubloc_addr = '%s' or (hubloc_id_url = '%s' or hubloc_hash = '%s')) and hubloc_network in ('zot6', 'activitypub') order by hubloc_id desc",
-				dbesc(str_replace('acct:', '', $url)),
+			$x = q("select * from xchan left join hubloc on xchan_hash = hubloc_hash where (hubloc_id_url = '%s' or hubloc_hash = '%s') and hubloc_network in ('zot6', 'activitypub') order by hubloc_id desc",
 				dbesc($url),
 				dbesc($url)
 			);
@@ -323,8 +322,7 @@ class HTTPSig {
 	static function get_webfinger_key($id, $force = false) {
 
 		if (!$force) {
-			$x = q("select * from xchan left join hubloc on xchan_hash = hubloc_hash where hubloc_addr = '%s' or hubloc_id_url = '%s' and hubloc_network in ('zot6', 'activitypub') order by hubloc_id desc",
-				dbesc(str_replace('acct:', '', $id)),
+			$x = q("select * from xchan left join hubloc on xchan_hash = hubloc_hash where hubloc_id_url = '%s' and hubloc_network in ('zot6', 'activitypub') order by hubloc_id desc",
 				dbesc($id)
 			);
 
@@ -370,8 +368,7 @@ class HTTPSig {
 
 	static function get_zotfinger_key($id, $force = false) {
 		if (!$force) {
-			$x = q("select * from xchan left join hubloc on xchan_hash = hubloc_hash where hubloc_addr = '%s' or hubloc_id_url = '%s' and hubloc_network = 'zot6' order by hubloc_id desc",
-				dbesc(str_replace('acct:', '', $id)),
+			$x = q("select * from xchan left join hubloc on xchan_hash = hubloc_hash where hubloc_id_url = '%s' and hubloc_network = 'zot6' order by hubloc_id desc",
 				dbesc($id)
 			);
 

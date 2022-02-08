85ad5355cf3d497287b60a8d902778cdb88ebeef
Mario <mario@mariovavti.com>
2022-02-08 20:13:19 +0000

    revert logging

--- Zotlabs/Lib/Activity.php
+++ Zotlabs/Lib/Activity.php
@@ -61,8 +61,6 @@ class Activity {
 		}
 
 		logger('fetch: ' . $url, LOGGER_DEBUG);
-		hz_syslog('fetch: ' . $url, LOGGER_DEBUG);
-		bt_syslog('fetch');
 
 		if (strpos($url, 'x-zot:') === 0) {
 			$x = ZotURL::fetch($url, $channel);
